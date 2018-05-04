bf4e9121d08bc70759080df6f60d0662c65d00db
remy peyre <remy.peyre05@gmail.com>
2018-05-04 17:40:46 +0200

    Added view desktop of section home

--- src/components/home.vue
+++ src/components/home.vue
@@ -5,19 +5,37 @@
             <div>1</div>
             <div>LOGO</div>
         </div>
+        <div class="content_home">
+            <div class="title_section_home">
+                Laissez vous guidez le temps
+                d’une promenade
+            </div>
 
-        <div class="title_section_home">
-            Laissez vous guidez le temps
-            d’une promenade
+            <div class="flex_display_gif">
+                <img class="size_gif" src="../assets/images/anim-iphone.gif">
+            </div>
+
+            <div class="btn_home">
+                découvrir
+            </div>
         </div>
 
-        <div class="flex_display_gif">
-            <img class="size_gif" src="../assets/images/anim-iphone.gif">
+        <div class="content_home_desktop">
+            <div>
+                <div class="title_section_home">
+                    Laissez vous guidez le temps
+                    d’une promenade
+                </div>
+                <div class="btn_home">
+                    découvrir
+                </div>
+            </div>
+
+            <div class="flex_display_gif">
+                <img class="size_gif" src="../assets/images/anim-iphone.gif">
+            </div>
         </div>
 
-        <div class="btn_home">
-            découvrir
-        </div>
 
     </section>
 </template>
@@ -50,6 +68,13 @@
         color: #fcfcfc;
         padding-bottom: 50px;
     }
+    .content_home{
+        display: flex;
+        flex-direction: column;
+    }
+    .content_home_desktop{
+        display: none;
+    }
     .title_section_home{
         font-weight: bold;
         font-size: 25px;
@@ -76,6 +101,21 @@
         margin: auto;
         font-family: "MES Bold";
         font-size: 16px;
+    }
+
+    @media (min-width: 900px) {
+        .content_home{
+            display: none;
+        }
+        .content_home_desktop{
+            display: flex;
+            width: 70%;
+            margin: auto;
+            align-items: center;
+        }
+        .btn_home{
+            margin-top: 55px;
+        }
     }
 
 </style>
