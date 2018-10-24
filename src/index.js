77a21be3571a7f0584db1b3be4d8e104c02c6e5b
remy peyre <remy.peyre05@gmail.com>
2018-10-24 21:32:42 +0200

    Added vue-nav-tabs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import Vue from 'vue/dist/vue.js'
 import App from './components/App.vue'
 import VueRouter from 'vue-router'
 import router from './router.js'
+import VueTabs from 'vue-nav-tabs'
+import 'vue-nav-tabs/themes/vue-tabs.css'
 
+Vue.use(VueTabs)
 Vue.use(VueRouter)
 
 Vue.config.productionTip = false
--- src/views/Documentation.vue
+++ src/views/Documentation.vue
@@ -2,23 +2,57 @@
     <div id="app">
         {{DOC}}
 
+        <vue-tabs>
+            <v-tab title="First tab"
+                   v-for="post in posts"
+                   v-bind:key="post.id"
+                   v-bind:title="post.title"
+            >
+                <div class="inlineInfoTabs">
+                    <div >1</div>
+                    <div>First tab content</div>
+                </div>
+
+
+            </v-tab>
+
+            <!--<v-tab title="Second tab">
+                Second tab content
+            </v-tab>-->
+
+        </vue-tabs>
+
     </div>
 </template>
 
 <script>
+    import {VueTabs, VTab} from 'vue-nav-tabs';
+    import 'vue-nav-tabs/themes/vue-tabs.css'
 
     export default {
-        name: 'app',
+        name: 'documentation',
         data: () => {
             return{
                 DOC: 'c\'est la doc',
+                posts: [
+                    { id: 1, title: 'Reac' },
+                    { id: 2, title: 'Vue' }
+                ]
             }
+        },
+        components: {
+            VueTabs,
+            VTab
         }
     }
 
 </script>
 
 <style scoped>
-
-
+.inlineInfoTabs{
+    display: flex;
+}
+.inlineInfoTabs div{
+    margin-right: 10px;
+}
 </style>
